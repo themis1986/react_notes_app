5d71513b665181b1d5ae699a5d93d84a75a31ab1
thiakovidis <thiakovidis@agroapps.gr>
2023-01-03 14:45:04 +0200

    Add javascript to project, fetch data from supabase db

--- data.js
+++ data.js
@@ -45,4 +45,4 @@ const initialFacts = [
 
 // LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
 
-// 👍 🤯 ⛔️
+// 👍 🤯 ⛔️ Themis_06091986
